3583101d7674af40cf4a253936f943ff2728150e
JasonW <50759798+TechnoSpeed@users.noreply.github.com>
2021-09-28 00:01:48 +1300

    Design Adjustments

--- JPH.py
+++ JPH.py
@@ -1,8 +1,9 @@
 #Program for AS91896 Internal
 from tkinter import *
+from tkinter import ttk
 
 root = Tk()
-root.geometry('500x200')
+root.geometry('780x300')
 root.title("Juile's Party Hire Tracker")
 
 #quit the program
@@ -15,7 +16,7 @@ def delete_text():
     global customer_name
     customer_name.destroy(1.0, END)
 
-#print the details from user
+#print the details from user(not in use)
 def print():
     Label(root, text=customer_name.get()).grid(row=4, column=1)
     Label(root, text=receipt_number.get()).grid(row=4, column=2)
@@ -28,29 +29,50 @@ def info():
     global customer_name, receipt_number, item_hired, amount_hired
     
     #Customer Name input
-    Label(root, text='Customer Name').grid(row=0, column=0)
+    Label(root, text='Customer Name').grid(row=0, column=0, padx=20, sticky=W)
     customer_name = Entry(root, width=20)
-    customer_name.grid(row=0, column=1)
+    customer_name.grid(row=1, column=0, padx=(20))
     
     #Receipt Number input
-    Label(root, text='Receipt Number').grid(row=1, column=0)
+    Label(root, text='Receipt Number').grid(row=0, column=1, sticky=W)
     receipt_number = Entry(root, width=20)
-    receipt_number.grid(row=1, column=1)
+    receipt_number.grid(row=1, column=1, padx=(0,20))
 
     #Item Hired input
-    Label(root, text='Item Hired').grid(row=2, column=0)
+    Label(root, text='Item Hired').grid(row=0, column=2, sticky=W)
     item_hired = Entry(root, width=20)
-    item_hired.grid(row=2, column=1)
+    item_hired.grid(row=1, column=2, padx=(0,20))
 
     #Amount Hired input
-    Label(root, text='Amount Hired').grid(row=3, column=0)
+    Label(root, text='Amount Hired').grid(row=0, column=3,sticky=W)
     amount_hired = Entry(root, width=20)
-    amount_hired.grid(row=3, column=1)
+    amount_hired.grid(row=1, column=3, padx=(0,20))
 
     #Buttons for print, delete & quit
-    Button(root, text="Print", command=print).grid(row=0, column=3, padx=30)
-    Button(root, text="Delete", command=delete_text).grid(row=1, column=3)
-    Button(root, text="Quit", command=quit).grid(row=2, column=3)
- 
+    Button(root, text="Print", command=quit).grid(row=1, column=4, padx=10, sticky=E)
+    Button(root, text="Delete", command=quit).grid(row=1, column=5, padx=10, sticky=E)
+    Button(root, text="Quit", command=quit).grid(row=1, column=6, padx=10, sticky=E)
+
+    #Create the table format
+    tree = ttk.Treeview(root, show='headings', height=8)
+    #Create the columns
+    tree['columns'] = ("Name","Receipt","Item", "Amount")
+    tree.column("#0", width=0, minwidth=0)
+    tree.column("Name", anchor=W, width=200)
+    tree.column("Receipt", anchor=W, width=150)
+    tree.column("Item", anchor=W, width=200)
+    tree.column("Amount", anchor=W, width=150)
+    #Create the headings 
+    tree.heading("#0", text="Label", anchor=W)
+    tree.heading("Name", text="Customer Name", anchor=W)
+    tree.heading("Receipt", text="Receipt Number", anchor=W)
+    tree.heading("Item", text="Hired Item", anchor=W)
+    tree.heading("Amount", text="Amount Item", anchor=W)
+    #Testing input values (not actual functional input)
+    tree.insert(parent='', index='end', iid=0, values=("John", 3264, "Table", 23))
+    tree.insert(parent='', index='end', iid=1, values=("Alex", "adc5462", "Chair", 832))
+
+    tree.place(x=20,y=80)
 info()
+
 root.mainloop()
